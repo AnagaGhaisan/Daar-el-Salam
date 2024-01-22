43dac9c0d7a23cd8278d19f91394351ee5130b75
Aga <mac@Aga-MacBook-Pro.local>
2024-01-22 14:00:53 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+        xx : "#CC7D55",
+        x : "#B1C5E1" ,
+        xxx : "#6CA5C1",
+        x1 : "#FFD600",
+        x2 : "#FF7A00",
+        x3 : "#FF0069",
+        x4 : "#D300C5",
+        x5 : "#7638FA",
+        y1 : "#FF0000",
+        f1 : "#4267B2",
+      },
+    },
+  },
+  plugins: [],
+}
+
